a1d524917873e5d77a22f4f0df29940f8cc63ea0
ccarlaa <carlacarlaclementino@gmail.com>
2023-05-03 11:45:33 -0300

    style with stimulus

--- demo/app/javascript/controllers/hello_controller.js
+++ demo/app/javascript/controllers/hello_controller.js
@@ -1,21 +1,37 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "count" ];
+  static targets = [ "count", "checkbox" ];
 
   connect() {
     this.countSelected();
     this.element.querySelectorAll("input[name='task_ids[]']").forEach((checkbox) => {
       checkbox.addEventListener("change", () => {
         this.countSelected();
+        this.change()
       });
     });
   }
 
   countSelected() {
-    const count = document.querySelectorAll("input[name='task_ids[]']:checked").length;
+    const count = document.querySelectorAll("input[name='task_ids[]']:checked").length;    
     this.countTarget.textContent = count;
   }
+
+  change() {
+    const boxesWithColor = document.querySelectorAll("input[name='task_ids[]']")
+    boxesWithColor.forEach(box => {
+      console.log(box.checked)
+      if(box.checked) {
+        const parentDiv = box.closest(".index-post")
+        parentDiv.style.border = "1.5px solid orange"
+      } else {
+        const parentDiv = box.closest(".index-post")
+        parentDiv.style.border = "none"
+      }
+    })
+  }
+
 }
 
 
